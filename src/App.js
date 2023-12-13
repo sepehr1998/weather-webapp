990e9a2484d67db7025673a6c6fd5bede2aad9de
Sepehr Samadi <samadisepehr@gmail.com>
2023-12-13 09:22:03 +0200

    [1]: project initiated [2]: sidebar component created [3]: weather API call implemented

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SideBar from './components/sidebar/sidebar.component';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='side-bar'>
+          <SideBar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
